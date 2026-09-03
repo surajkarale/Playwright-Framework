694bbf31cc579c06d5dce55c2e9d22f6dbdfdc2e
Suraj <karale.surajraje@gmail.com>
2026-09-03 20:54:17 +0530

    Pushing to feature file

--- tests/google.spec.js
+++
@@ -1,7 +0,0 @@
-import {test,expect} from '@playwright/test'
-
-test('google', async ({ page }) => {
-
-    await page.goto('https://www.google.com/')
-    await expect(page).toHaveTitle(/Google/)
-})
